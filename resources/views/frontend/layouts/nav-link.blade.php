845b55c58565d2d948e89899580db1e4132a888f
popcorn09 <69863496+popcorn09@users.noreply.github.com>
2023-06-05 10:59:41 +0800

    Changes to nav-link

--- resources/views/frontend/layouts/nav-link.blade.php
+++ resources/views/frontend/layouts/nav-link.blade.php
@@ -1,22 +1,36 @@
 <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
   <li class="nav-item">
-    <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="{{ url('') }}">Home</a>
+    <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="{{ url('') }}">
+      {{ __('Home') }}
+    </a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ Request::is('about-us') ? 'active' : '' }}" href="{{ url('about-us') }}">About us</a>
+    <a class="nav-link {{ Request::is('about-us') ? 'active' : '' }}" href="{{ url('about-us') }}">
+      {{ __('About Us') }}
+    </a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ Request::is('our-services') ? 'active' : '' }}" href="{{ url('our-services') }}">Our Services</a>
+    <a class="nav-link {{ Request::is('our-services') ? 'active' : '' }}" href="{{ url('our-services') }}">
+      {{ __('Our Services') }}
+    </a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ Request::is('faqs') ? 'active' : '' }}" href="{{ url('faqs') }}">FAQs</a>
+    <a class="nav-link {{ Request::is('faqs') ? 'active' : '' }}" href="{{ url('faqs') }}">
+      {{ __('FAQs') }}
+    </a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ Request::is('blogs') ? 'active' : '' }}" href="{{ url('blogs') }}">Blogs</a>
+    <a class="nav-link {{ Request::is('blogs') ? 'active' : '' }}" href="{{ url('blogs') }}">
+      {{ __('Blogs') }}
+    </a>
   </li>
   <li class="nav-item">
-    <a class="nav-link {{ Request::is('contact-us') ? 'active' : '' }}" href="{{ url('contact-us') }}">Contact Us</a>
+    <a class="nav-link {{ Request::is('contact-us') ? 'active' : '' }}" href="{{ url('contact-us') }}">
+      {{ __('Contact Us') }}
+    </a>
   </li>
 </ul>
 <hr class="d-block d-lg-none">
-<a href="{{ url('track') }}" class="btn btn-1 fs-14px">TRACK YOUR PARCEL</a>
+<a href="" class="btn btn-1 fs-14px">
+  {{ __('TRACK YOUR PARCEL') }}
+</a>
